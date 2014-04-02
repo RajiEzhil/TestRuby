a4a2fe68ddfcb568136c6e4aa6fb6e2a2a3e2a6e
RajiEzhil <rajiezhil@gmail.com>
2014-04-02 18:11:01 +0530

    slam book test app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slambook::Application.config.secret_token = '2d9f0b4bbf61e57313783f2f55f7f2ecb066dae19216f173711248f70174b1f783ce977c435f04eb33d79d53d501dfcff1526751cd7a0fd2c15e61319cea3ff5'
